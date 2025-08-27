c3a70c91822a69310be8ff60eefd139e26aa73cd
sylvainduval <sylv1duval@proton.me>
2025-08-27 13:06:29 +0200

    restore getContainerExtension

--- src/Bridge/Symfony/DynamicDbSymfonyBundle.php
+++ src/Bridge/Symfony/DynamicDbSymfonyBundle.php
@@ -5,5 +5,13 @@ declare(strict_types=1);
 namespace SylvainDuval\DynamicDbBundle\Bridge\Symfony;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use SylvainDuval\DynamicDbBundle\DependencyInjection\DynamicDbExtension;
 
-class DynamicDbSymfonyBundle extends Bundle {}
+
+class DynamicDbSymfonyBundle extends Bundle {
+	public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new DynamicDbExtension();
+    }
+}
